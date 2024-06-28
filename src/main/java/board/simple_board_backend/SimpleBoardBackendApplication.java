9fb2e3e1f27c1c19aec6e32a5d5be0dfee10d0f0
함민진 <hammj5296@naver.com>
2024-06-28 16:06:46 +0900

    first commit

---
+++ src/main/java/board/simple_board_backend/SimpleBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package board.simple_board_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardBackendApplication.class, args);
+    }
+
+}
